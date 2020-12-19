8319e54dca15ba596941012c5c7fc524bd0e7b1d
theofrag <tho.fragoulis@gmail.com>
2020-12-19 18:45:42 +0200

    Lfatigue changed name to Ls/Lt/Lj

--- modules/human.cpp
+++ modules/human.cpp
@@ -2,15 +2,15 @@
 
 void Junior::attend(int hours){
     for(int i=0;i<hours;i++)
-            this->fatigue = this->fatigue + this->Lfatigue;
+            this->fatigue = this->fatigue + this->Lj;
 }
 
 void Senior::attend(int hours){
     for(int i=0;i<hours;i++)
-            this->fatigue = this->fatigue + this->Lfatigue;
+            this->fatigue = this->fatigue + this->Ls;
 }
 
 void Teacher::teach(int hours){
     for(int i=0;i<hours;i++)
-        this->fatigue = this->fatigue + this->Lfatigue; 
+        this->fatigue = this->fatigue + this->Lt; 
 }
